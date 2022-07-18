40a61a066c88aa6242a16756f8cfa1d67b02699d
faddal <faddal.ibrahim@turntabl.io>
2022-07-18 16:58:23 +0000

    max prod:

--- heaps/max-prod-2-elements.py
+++ heaps/max-prod-2-elements.py
@@ -12,3 +12,17 @@ class Solution:
         y = heapq.heappop(nums)
         
         return (x-1)*(y-1)
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        heap = []
+        
+        for num in nums:
+            if len(heap) < 2:
+                heapq.heappush(heap,num)
+            else:
+                heapq.heappush(heap,num)
+                heapq.heappop(heap)
+                
+        return (heap[0] - 1) * (heap[1] - 1)
