20b76ee89413cc02f5e443dd17c571f1625cd238
faddal <faddal.ibrahim@turntabl.io>
2022-07-20 16:08:45 +0000

    peoples solutions

--- heaps/subsequence-k-largest-sum.py
+++ heaps/subsequence-k-largest-sum.py
@@ -99,3 +99,38 @@ class Solution:
 #             final.append(heappop(pairs)[1])
             
 #         return final
+
+
+# someones solutions
+
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        tuple_heap = [] # Stores (value, index) as min heap
+        for i, val in enumerate(nums):
+            if len(tuple_heap) == k:
+                heappushpop(tuple_heap, (val, i)) # To prevent size of heap growing larger than k
+            else:
+                heappush(tuple_heap, (val, i))
+		# heap now contains only the k largest elements with their indices as well.
+        tuple_heap.sort(key=lambda x: x[1]) # To get the original order of values. That is why we sort it by index(x[1]) & not value(x[0])
+        ans = []
+        for i in tuple_heap:
+            ans.append(i[0])
+        return ans
+
+
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        # Heap to keep track of max values, and map to output ordering by index
+        pq = []
+        heapq.heapify(pq)
+        indexes = {}
+        for index, num in enumerate(nums):
+            # Add all values to heap and map
+            heapq.heappush(pq, (num, index))
+            indexes[index] = num
+            # If we have more values than can be returned, remove the smallest from both the heap and the map
+            if len(pq) > k:                
+                del indexes[heapq.heappop(pq)[1]]
+        # Return the values in the map, which represent the answers ordered in this case
+        return indexes.values()
